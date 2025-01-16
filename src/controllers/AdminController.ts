bf327600ede258174a83c3c964dcd2c8db5a5978
ranveer1singh <ranveersinghtomar2000@gmail.com>
2025-01-16 10:34:05 +0530

    new changes in admin controller

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -13,13 +13,14 @@ export const FindVandor = async (id:string | undefined, email? : string ) => {
 }
 
 // Create 
-export const CreateVandor = async (req: Request, res: Response, next: NextFunction) => {
+export const CreateVandor = async (req: Request, res: Response, next: NextFunction) : Promise<void>=> {
     try {
         const { name, ownerName, foodType, pincode, address, phone, email, password } = req.body;
 
         const existingVandor = await FindVandor("", email);
         if (existingVandor != null) {
               res.json({ message: "vandor already exists" });
+              return;
            
         }
 
@@ -43,23 +44,29 @@ export const CreateVandor = async (req: Request, res: Response, next: NextFuncti
         });
 
         res.json(createVandor); // Respond with the newly created vendor
+        return ;
     } catch (error) {
         console.log(error); // Forward errors to the error handler
+        res.status(500).json({
+            message : "Internal server error" + error
+        })
     }
 };
 
-export const GetVandor = async (req: Request, res: Response, next: NextFunction) => {
+export const GetVandor = async (req: Request, res: Response, next: NextFunction) : Promise<void>=> {
     try {
         const vandors = await Vandor.find();
 
         if (vandors && vandors.length > 0) {
               res.status(200).json(vandors); //   200 status with the data
+              return;
         }
 
         // If no vandors are found
           res.status(404).json({
             message: "No vandors found",
         });
+        return ;
     } catch (error) {
         // Handle any unexpected errors
         console.error("Error fetching vandors:", error);
@@ -67,9 +74,10 @@ export const GetVandor = async (req: Request, res: Response, next: NextFunction)
             message: "An error occurred while fetching vandors",
             error: error.message || "Internal Server Error",
         });
+        return ;
     }
 };
-export const GetVandorById =  async (req : Request, res : Response, next: NextFunction)=>{
+export const GetVandorById =  async (req : Request, res : Response, next: NextFunction) : Promise<void>=>{
 
     const vandorId = req.params.id;
 
@@ -77,6 +85,7 @@ export const GetVandorById =  async (req : Request, res : Response, next: NextFu
     // Validate if ID is provided
     if (!vandorId) {
        res.status(400).json({ message: 'Vendor ID is required' });
+       return ;
     }
 
     // Call the service or database query to find the vendor
@@ -84,14 +93,22 @@ export const GetVandorById =  async (req : Request, res : Response, next: NextFu
 
     // Check if the vendor was found
     if (vandor) {
-       res.status(200).json(vandor);
+       res.status(200).json({
+        message :  "Record found",
+        data : vandor});
+       return ;
     }
 
     // Vendor not found
      res.status(404).json({ message: 'Vendor not found' });
+     return ;
   } catch (error) {
     // Handle unexpected errors
     next(error);
+    res.status(500).json({
+        message : "Internal server error" + error
+    })
+    return ;
   }
 }
 
